a0a7c967a09920e9e0b900b4bfa4df9521d7962c
Wade Deng <no7david@gmail.com>
2020-01-09 19:46:16 +0800

    Update Copy.py

--- macOS/Copy.py
+++ macOS/Copy.py
@@ -1,4 +1,6 @@
-if window.get_active_class() != 'gnome-terminal-server.Gnome-terminal':
-    keyboard.send_keys("<ctrl>+c")
+terminals = [ 'terminator.Terminator', 'gnome-terminal.Gnome-terminal' ]
+winClass = window.get_active_class()
+if winClass in terminals:
+    keyboard.send_keys('<ctrl>+<shift>+c')
 else:
-    keyboard.send_keys("<ctrl>+<shift>+c")
+    keyboard.send_keys('<ctrl>+c')
