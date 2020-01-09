9ebd8f5cc3c9ba64883a55c35ed04e5215912d5f
Wade Deng <no7david@gmail.com>
2020-01-09 19:46:42 +0800

    Update Paste.py

--- macOS/Paste.py
+++ macOS/Paste.py
@@ -1,4 +1,6 @@
-if window.get_active_class() != 'gnome-terminal-server.Gnome-terminal':
-    keyboard.send_keys("<ctrl>+v")
+terminals = [ 'terminator.Terminator', 'gnome-terminal.Gnome-terminal' ]
+winClass = window.get_active_class()
+if winClass in terminals:
+    keyboard.send_keys('<ctrl>+<shift>+v')
 else:
-    keyboard.send_keys("<ctrl>+<shift>+v")
+    keyboard.send_keys('<ctrl>+v')
