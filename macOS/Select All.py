8a677e4bceea14e1e671861c8905281af78dfb32
Wade Deng <no7david@gmail.com>
2020-01-16 10:15:33 +0800

    Update Select All.py

--- macOS/Select All.py
+++ macOS/Select All.py
@@ -1,6 +1,6 @@
 terminals = [ 'terminator.Terminator', 'gnome-terminal.Gnome-terminal' ]
 winClass = window.get_active_class()
-if window.get_active_class() != 'gnome-terminal-server.Gnome-terminal':
+if winClass in terminals:
     keyboard.send_keys("<ctrl>+a")
 else:
     keyboard.send_keys("<ctrl>+<shift>+a")
