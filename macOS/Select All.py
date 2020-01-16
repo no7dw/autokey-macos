1c28dc0293a3a502d950bbde1454238db14f6782
Wade Deng <no7david@gmail.com>
2020-01-16 10:15:07 +0800

    Update Select All.py

--- macOS/Select All.py
+++ macOS/Select All.py
@@ -1,3 +1,5 @@
+terminals = [ 'terminator.Terminator', 'gnome-terminal.Gnome-terminal' ]
+winClass = window.get_active_class()
 if window.get_active_class() != 'gnome-terminal-server.Gnome-terminal':
     keyboard.send_keys("<ctrl>+a")
 else:
