9b5cce0781806ae1fc0bb21bcdfb5223a6c2c23f
Wade Deng <no7david@gmail.com>
2020-01-16 10:16:04 +0800

    Update Cut.py

--- macOS/Cut.py
+++ macOS/Cut.py
@@ -1,6 +1,7 @@
 terminals = [ 'terminator.Terminator', 'gnome-terminal.Gnome-terminal' ]
 winClass = window.get_active_class()
-if window.get_active_class() != 'gnome-terminal-server.Gnome-terminal':
+if winClass in terminals:
+
     keyboard.send_keys("<ctrl>+x")
 else:
     keyboard.send_keys("<ctrl>+<shift>+c")
